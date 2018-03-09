951463f029dded77e6988643bfab814e6b6df93f
Rayenedi Ganga Bhavani <gangabhavanirayenedi@gmail.com>
2018-03-09 10:51:02 +0100

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/example/venka/icecreamorderapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/venka/icecreamorderapp/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.venka.icecreamorderapp", appContext.getPackageName());
+        assertEquals("c om.example.venka.icecreamorderapp", appContext.getPackageName());
     }
 }
